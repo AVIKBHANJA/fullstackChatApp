ff9851594164d1886109ade7ea350ee511b35c56
AVIKBHANJA <avikbhanja3@gmail.com>
2025-07-09 18:36:12 +0530

    Refactor CORS configuration to improve readability and maintainability

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,12 +18,31 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin:process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin:process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
+// const corsOptions = {
+//   origin:
+//     process.env.NODE_ENV === "production"
+//       ? process.env.FRONTEND_URL || true // Allow any origin in production if not specified
+//       : "http://localhost:5173",
+//   credentials: true,
+// };
+
+// app.use(cors(corsOptions));
+const corsOptions = {
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.FRONTEND_URL || true // Allow any origin in production if not specified
+      : "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
